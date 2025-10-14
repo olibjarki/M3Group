498e6e06b7167c42dd040fe8adc5c16be183e0ed
qajqaasulimqaa <kaja.sulima@gmail.com>
2025-10-14 14:57:30 +0000

    merging

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ if (train) {
     function move() {
         position_1 -= speed_1;
         train.style.transform = "translateX(".concat(position_1, "px)");
-        var resetPoint = train.scrollWidth / 4;
+        var resetPoint = train.scrollWidth;
         if (Math.abs(position_1) >= resetPoint) {
             position_1 = 0;
         }
@@ -19,15 +19,16 @@ var train2 = document.getElementById("book-train-2");
 if (train2) {
     // Clone all books once automatically for seamless loop
     train2.innerHTML += train2.innerHTML;
-    var contentWidth_1 = train2.scrollWidth / 2;
-    var position2_1 = contentWidth_1; // Start offscreen to left
+    var contentWidth_1 = train2.clientWidth;
+    console.log(contentWidth_1);
+    var position2_1 = contentWidth_1 * -1; // Start offscreen to left
     var speed2_1 = 0.5;
     function move2() {
         position2_1 += speed2_1; // Move to the right
         train2.style.transform = "translateX(".concat(position2_1, "px)");
         // Reset once full loop completes
         if (position2_1 >= 0) {
-            position2_1 = -contentWidth_1;
+            position2_1 = contentWidth_1 * -1;
         }
         requestAnimationFrame(move2);
     }
