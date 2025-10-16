9756a16dd00d6ea0a5b068f838236e59187dbaa7
qajqaasulimqaa <kaja.sulima@gmail.com>
2025-10-16 21:57:48 +0000

    Final changes to the main animation on landing page

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ var train = document.getElementById("book-train");
 if (train) {
     // Clone all books once automatically for seamless loop
     train.innerHTML += train.innerHTML;
-    var position_1 = 0;
+    var position_1 = -200;
     var speed_1 = 0.5;
     function move() {
         position_1 -= speed_1;
@@ -17,16 +17,17 @@ if (train) {
 }
 var train_2 = document.getElementById("book-train2");
 if (train_2) {
-    // Clone all books for seamless loop
     train_2.innerHTML += train_2.innerHTML;
-    var position2_1 = 0;
+    console.log(train_2.scrollWidth);
+    var position2_1 = -1500;
     var speed2_1 = 0.5;
     function move_2() {
-        position2_1 -= speed2_1;
+        position2_1 += speed2_1;
+        console.log(position2_1);
         train_2.style.transform = "translateX(".concat(position2_1, "px)");
         var resetPoint = train_2.scrollWidth;
-        if (Math.abs(position2_1) >= resetPoint) {
-            position2_1 = 0;
+        if (position2_1 >= resetPoint) {
+            position2_1 = -1300;
         }
         requestAnimationFrame(move_2); // ✅ Correct function name
     }
